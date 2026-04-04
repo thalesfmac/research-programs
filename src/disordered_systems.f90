66595fe7b1e74b03b1ddd459982abec74711e6d8
Thales Freitas Macêdo <thalesfreitas96@gmail.com>
2026-04-04 00:58:36 -0300

    aa_onsite_potential added

--- src/disordered_systems.f90
+++ src/disordered_systems.f90
@@ -16,5 +16,24 @@ module disordered_systems
         V_i = V * cos(2.0_dp * PI * beta * real(site, kind=dp) + phi)
     end function aa_onsite_potential
 
+    subroutine aa_slice_hamiltonian(h_i, i, V, beta, phi, omega)
+        integer, intent(in) :: i
+        real(dp), intent(in) :: V, beta, phi, omega
+        real(dp), intent(out) ::  h_i(0:, 0:)
+
+        integer :: Nph, n
+
+        if (ubound(h_i,1) /= ubound(h_i,2)) then
+            error stop "aa_slice_hamiltonian: h_i must be square"
+        end if
+        Nph = ubound(h_i, dim=1)
+
+        h_i = 0.0_dp
+
+        do n = 0, Nph
+            h_i(n, n) = aa_onsite_potential(V, i, beta, phi) + real(n, kind=dp) * omega
+        end do
+    end subroutine aa_slice_hamiltonian
+
 
 end module disordered_systems
