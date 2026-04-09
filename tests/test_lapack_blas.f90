244227c34d0d4c9a3b6f8b78c11c72ff47d0996f
Thales Freitas Macêdo <thalesfreitas96@gmail.com>
2026-04-08 22:02:48 -0300

    preliminary lapack_blas test added

---
+++ tests/test_lapack_blas.f90
@@ -0,0 +1,28 @@
+program test_lapack_blas
+    use precision, only : dp
+    use lapack_blas
+    use matrix_operations, only : identity_matrix
+    implicit none
+
+    complex(dp), allocatable :: A(:,:), B(:,:), C(:,:), I_ref(:,:)
+    real(dp), allocatable :: w(:)
+    real(dp) :: tol = 1.0e-12_dp
+
+    ! --- Test Inversion ---
+    allocate(A(2,2), I_ref(2,2))
+    call identity_matrix(I_ref)
+    A(1,1) = (2.0_dp, 0.0_dp); A(1,2) = (0.0_dp, 1.0_dp)
+    A(2,1) = (0.0_dp, -1.0_dp); A(2,2) = (2.0_dp, 0.0_dp)
+
+    B = A ! Keep copy
+    call invert(B)
+    call matmul2(A, B, C)
+
+    if (maxval(abs(C - I_ref)) < tol) then
+        print *, "Invert test: PASSED"
+    else
+        print *, "Invert test: FAILED"
+    end if
+
+    ! Add similar blocks for matmul2 and diagonalize...
+end program test_lapack_blas
