50b6ec96a274bbe9ca47e0b59a9ac85f55febb5a
Thales Freitas Macêdo <thalesfreitas96@gmail.com>
2026-05-12 17:29:29 -0300

    cavityaa_rgf now do both energy and length sweps

--- app/cavityaa_rgf.f90
+++ app/cavityaa_rgf.f90
@@ -7,71 +7,88 @@ program main
     implicit none
 
     character(len=256) :: outname
-    integer :: Lx, Nph, seed, Ndisorder, NEpoints
+    integer :: Lx, Nph, seed, Ndisorder, NEpoints, NLpoints
     real(dp) :: t, V
     real(dp) :: tcS, tcD, tlead, muS, muD
     real(dp) :: omega, g
 
-    real(dp), allocatable :: energies(:), phis(:), transmissions(:, :)
+    real(dp), allocatable :: energies(:), phis(:), lengths(:)
+    real(dp), allocatable :: transmissions(:, :, :)
     real(dp) :: Emin, Emax
+    integer :: Lmin, Lmax
     real(dp), parameter :: ETA = 1.0e-10_dp
 
-    integer :: i, j
+    integer :: i, j, k
+    character(len=32) :: jstr
 
     call readInput()
+
+    NLpoints = Lmax - Lmin + 1
+    if (NLpoints <= 0) then
+        error stop "main: Lmax must be greater than or equal to Lmin"
+    end if
 
     call writeInput("parameters_" // trim(outname) // ".txt")
 
     call rng_initialize(seed)
 
+    allocate(lengths(NLpoints))
     allocate(energies(NEpoints))
     allocate(phis(Ndisorder))
-    allocate(transmissions(NEpoints, Ndisorder))
+    allocate(transmissions(NLpoints, NEpoints, Ndisorder))
 
-    Emin = max(muS - 2.0_dp*tlead, muD - 2.0_dp*tlead)
-    Emax = min(muS + 2.0_dp*tlead, muD + 2.0_dp*tlead)
-
-    call energy_grid( &
-        Egrid = energies, &
-        Emin = max(muS - 2.0_dp*tlead, muD - 2.0_dp*tlead), &
-        Emax = min(muS + 2.0_dp*tlead, muD + 2.0_dp*tlead))
+    call energy_grid(Egrid = energies, Emin = Emin, Emax = Emax)
     call aa_random_phases(phis)
 
-    do i = 1, NEpoints
-        do j = 1, Ndisorder
-            transmissions(i, j) = cavaa_rgf_transmission( &
-                E      = energies(i),  &
-                eta    = ETA,          &
-                Lx     = Lx,           &
-                Nph    = Nph,          &
-                t      = t,            &
-                V      = V,            &
-                beta   = INV_PHI,      &
-                phi    = phis(j),      &
-                g      = g,            &
-                omega  = omega,        &
-                tcL    = tcS,          &
-                tcR    = tcD,          &
-                tlead  = tlead,        &
-                muL    = muS,          &
-                muR    = muD )
-        end do
+    do k = 1, NLpoints
+        Lx = Lmin + k - 1
+        lengths(k) = real(Lx, dp)
 
-        write(*, *) "Energy = ", energies(i)
+        do i = 1, NEpoints
+            do j = 1, Ndisorder
+                transmissions(k, i, j) = cavaa_rgf_transmission( &
+                    E      = energies(i),  &
+                    eta    = ETA,          &
+                    Lx     = Lx,           &
+                    Nph    = Nph,          &
+                    t      = t,            &
+                    V      = V,            &
+                    beta   = INV_PHI,      &
+                    phi    = phis(j),      &
+                    g      = g,            &
+                    omega  = omega,        &
+                    tcL    = tcS,          &
+                    tcR    = tcD,          &
+                    tlead  = tlead,        &
+                    muL    = muS,          &
+                    muR    = muD )
+            end do
+
+            write(*, *) "L = ", Lx
+        end do
     end do
 
     call save_array_1d("energies_" // trim(outname) // ".dat", energies)
-    call save_array_2d("transmissions_" // trim(outname) // ".dat", transmissions)
-    ! call save_array_bin("transmissions_" // trim(outname) // ".bin", transmissions)
+    call save_array_1d("lengths_" // trim(outname) // ".dat", lengths)
 
-    deallocate(energies, phis, transmissions)
+    do j = 1, Ndisorder
+        write(jstr, '(I4.4)') j
+
+        call save_array_2d("transmissions_" // trim(outname) // "_" // trim(jstr) //".dat", transmissions(:, :, j))
+        ! call save_array_bin("transmissions_" // trim(outname) // ".bin", transmissions)
+    end do
+
+
+    deallocate(lengths, energies, phis, transmissions)
 
     contains
 
     subroutine readInput()
         use, intrinsic :: iso_fortran_env, only : input_unit
         read(input_unit,*) outname
-        read(input_unit,*) Lx, NEpoints, Ndisorder
+        read(input_unit,*) NEpoints, Ndisorder
+        read(input_unit,*) Lmin, Lmax
+        read(input_unit,*) Emin, Emax
         read(input_unit,*) Nph
         read(input_unit,*) seed
         read(input_unit,*) t, V
@@ -87,10 +104,14 @@ program main
 
         write(unit, *) "Input data"
         write(unit, *) "outname=", trim(outname)
-        write(unit, *) "Lx=", Lx
+        write(unit, *) "Size grid=", NLpoints
+        write(unit, *) "Lmin=", Lmin
+        write(unit, *) "Lmax=", Lmax
         write(unit, *) "Nph=", Nph
         write(unit, *) "seed=", seed
         write(unit, *) "Energy grid=", NEpoints
+        write(unit, *) "Emin=", Emin
+        write(unit, *) "Emax=", Emax
         write(unit, *) "Number of disorder conf.=", Ndisorder
         write(unit, *) "t=", t
         write(unit, *) "V=", V
